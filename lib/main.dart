3d480cb8c2ab67fd4cad2f7c95c1e6b44ce1d274
moeyad7 <mohamed.eyad2612@gmail.com>
2023-04-29 22:22:05 +0300

    video 10 folder 14

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import './screens/auth_screen.dart';
 import './screens/chat_screen.dart';
-void main() async{
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Chat',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }
-
