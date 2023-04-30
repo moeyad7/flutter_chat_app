3cfae27735266ccb741317124ccbf4602586f34b
moeyad7 <mohamed.eyad2612@gmail.com>
2023-04-30 19:17:51 +0300

    video 20 folder 14

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../widgets/chat/new_message.dart';
 import '../widgets/chat/messages.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -45,18 +46,9 @@ class ChatScreen extends StatelessWidget {
             Expanded(
               child: Messages(),
             ),
+            NewMessage(),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/mnoH2T214xGHnBE2ADVY/messages')
-              .add({
-            'text': 'This was added by clicking the button!',
-          });
-        },
       ),
     );
   }
--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -7,13 +7,17 @@ class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection("chat").snapshots(),
+      stream: FirebaseFirestore.instance
+          .collection("chat")
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
       builder: (ctx, chatSnapshot) {
         if (chatSnapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
         }
         final chatDocs = chatSnapshot.data.docs;
         return ListView.builder(
+          reverse: true,
           itemCount: chatDocs.length,
           itemBuilder: (ctx, index) => Text(chatDocs[index]["text"]),
         );
