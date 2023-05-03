fffdb167679de717fc41c240ffd613f0eb7b644d
moeyad7 <mohamed.eyad2612@gmail.com>
2023-05-03 13:21:03 +0300

    video 31 folder 14

--- lib/widgets/chat/new_message.dart
+++ lib/widgets/chat/new_message.dart
@@ -16,16 +16,15 @@ class _NewMessageState extends State<NewMessage> {
   void _sendMessage() async {
     FocusScope.of(context).unfocus();
     final user = await FirebaseAuth.instance.currentUser;
-    final userData = await FirebaseFirestore.instance
-        .collection('user')
-        .doc(user.uid)
-        .get();
-        
+    final userData =
+        await FirebaseFirestore.instance.collection('user').doc(user.uid).get();
+
     FirebaseFirestore.instance.collection("chat").add({
       'text': _enteredMessage,
       'createdAt': Timestamp.now(),
       'userId': user.uid,
       'username': userData['username'],
+      'userImage': userData['image_url'],
     });
     _controller.clear();
     _enteredMessage = '';
